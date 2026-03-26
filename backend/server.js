50721d974a764a80ae91c7bd494907ec70d314d3
Gireesh Kumar Gowd <gireeshkumargowd@gmail.com>
2026-03-26 14:29:57 +0530

    fixed the cors error

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Enable CORS - explicitly allow frontend
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:5174',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
